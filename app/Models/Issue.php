e7aa54192ebcd64839048527fcc54e612df1b4b2
James McCormack <james@wirebox.co.uk>
2025-03-26 21:59:16 +0000

    issue fillable

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Issue extends Model
 {
-    protected $fillable = [];
+    protected $fillable = ['title', 'description'];
 
     public function project()
     {
